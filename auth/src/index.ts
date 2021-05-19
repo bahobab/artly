a363623e1812d5e0c89d267d246acc630c0e9292
Konan Houphoue <bahobab@hotmail.com>
2021-05-18 20:23:52 -0500

    created auth-mongo-depl. created auth database

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -24,7 +25,21 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+const startServer = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('connected to mongodb')
+  } catch (error) {
+    console.error(error)
+  }
+  
+  app.listen(3000, () => {
+    console.log('AUTH: Server listening on port 3000!!!');
+  });
+};
 
-app.listen(3000, () => {
-  console.log('AUTH: Server listening on port 3000!!');
-});
+startServer();
