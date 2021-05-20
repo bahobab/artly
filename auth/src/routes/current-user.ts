d09a11b3870737795ea5c432bea67657570d5754
Konan Houphoue <bahobab@hotmail.com>
2021-05-19 20:38:52 -0500

    implemented current-user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router();
 
-router.get('/api/users/current-user', (req, res) => {
-  const { username, password } = req.body;
-  res.send('Current User');
+router.get('/api/users/currentuser', (req, res) => {
+  if (!req.session?.jwt) { // equivalernt to !req.session ||!req.session.jwt
+    res.send({ currentUser: null });
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!)
+    res.send({currentUser: payload});
+    
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
