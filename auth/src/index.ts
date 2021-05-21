551293f4a39e2ae28bf301f93da812eb9aaac082
Konan Houphoue <bahobab@hotmail.com>
2021-05-20 21:28:50 -0500

    refactor index.ts to facilitate testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from 'express';
-import cookieSession from 'cookie-session';
-import 'express-async-errors';
 import mongoose from 'mongoose';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from '../src/middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-
-app.set('trust proxy', true); // because of ingress-nginx
-
-app.use(express.json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}));
-
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const startServer = async () => {
   if (!process.env.JWT_KEY) {
